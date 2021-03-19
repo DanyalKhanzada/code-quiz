5ae854cbef860f51fc8ae847f1997f33d6d822f6
“Danyal <danyalkhanzada89@gmail.com>
2021-03-19 17:50:37 -0400

    Questions finalized

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+
+
+
+var codeQuestions = [
+    {
+      question: "An if/else statement is enclosed with:",
+      possibleAnswers: ["Parentheses", "Curly Brackets", "Square Brackets", "Quotations"],
+      correctAnswer: "Curly Brackets" 
+    },
+    {
+      question: "To target HTML element, you would target it by its:",
+      possibleAnswers: ["Class", "Title", "ID", "Semantic tag"],
+      correctAnswer: "ID"
+    },
+    {
+      question: "Where is the right place to put a Javascript?",
+      possibleAnswers: ["The head section", "The body section", "Neither the body or head sections", "Either the body or head sections"],
+      correctAnswer: "The body section"
+    },
+    {
+      question: "Which value is not a falsy value?",
+      possibleAnswers: ["False", "Null", "Undefined", "Unknown"],
+      correctAnswer: "Unknown" 
+    },
+    {
+      question: "Where should you declare global variables?",
+      possibleAnswers: ["At the beginning of the .js file", "Inside a variable", "At the end of the .js file", "Nowhere"],
+      correctAnswer: "At the beginning of the .js file" 
+    },
+    {
+    question: "What is the proper way to set alternate text for an image element?",
+    possibleAnswers: ["<img src='picture.jpg' alt='This is my alternate text' />", "<img src='picture.jpg' text='This is my alternate text' />", "<img src='picture.jpg' img='This is my alternate text' />", "<img src='picture.jpg' caption='This is my alternate text' />"],
+    correctAnswer: "<img src='picture.jpg' alt='This is my alternate text' />" 
+    },
+    {
+    question: "Data stored in sessionStorage will persist after the window is closed. (True or False)",
+    possibleAnswers: ["True", "False", "", ""],
+    correctAnswer: "False" 
+    },
+    {
+    question: "Where is the correct place to link a stylesheet?",
+    possibleAnswers: ["At the top of the <body> tag", "At the bottom of the <body> tag", "Between the <head> tags", "Between the <title> tags"],
+    correctAnswer: "Between the <head> tags" 
+    }
+    {
+    question: "What is the proper way to style all <button> and all <a> elements?",
+    possibleAnswers: ["a button {\r\n  border: 2px\r\n}", "a:button {\r\n  border: 2px\r\n}", "button, a {\r\n  border: 2px\r\n}", "button a {\r\n  border: 2px\r\n}"],
+    correctAnswer: "button, a {\r\n  border: 2px\r\n}" 
+    }
+    {
+    question: "Which of the following is a JavaScript object?",
+    possibleAnswers: ["var newBook = {title: 'Lion King'};", "var newBook = 'Charlie and the chocolate factory';", "var newBook = ['title', 'the day i dreamt about sports'];", "var newBook = [title: 'chickenhead'];"],
+    correctAnswer: "var newBook = {title: 'Lion King'}" 
+    }
+
+  ];
